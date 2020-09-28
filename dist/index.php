ffe6947efc5a8cf4f4a0a6db8f92d5b158e79c0b
Janez Troha <dz0ny@ubuntu.si>
2020-09-28 11:36:45 +0200

    first commit

---
+++ dist/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: WooCart example of deploy
+ * Description: This is a test that show WooCart deploy.
+ * Version: 1.0
+ * Author: WooCart
+ */
+
+namespace WooCart {
+	
+
+}
